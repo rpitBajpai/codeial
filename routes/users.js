aeea32bd678d92cf62ff3e3ecc4dbc387de23fc4
BAJPAI <arpit.bajpai@accenture.com>
2022-05-21 01:24:51 +0530

    Created & Rendered SignIn/SignUp page

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,10 @@ const router = express.Router();
 const userController = require('../controllers/users_controller');
 const postController = require('../controllers/post_controller');
 
+
 router.get('/profile', userController.profile);
 router.get('/post', postController.post);
+router.get('/sign-up', userController.signUp );
+router.get('/sign-in', userController.signIn );
 
 module.exports = router;
