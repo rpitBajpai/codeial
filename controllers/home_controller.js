1e5cf87c799535bff3231a0e9ad1256401c5eb0e
BAJPAI <arpit.bajpai@accenture.com>
2022-05-24 00:22:33 +0530

    created user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,6 @@
 module.exports.home = function(req,res){
-
+    console.log(req.cookies);
+    res.cookie('user_id', 25);
 //    return res.end('<h1>Express is up for Codeial!</h1>'); -------sending data to browser directly
 
     return res.render('home', {
--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
-const userController = require('../controllers/users_controller');
+const usersController = require('../controllers/users_controller');
 const postController = require('../controllers/post_controller');
 
 
-router.get('/profile', userController.profile);
+router.get('/profile', usersController.profile);
 router.get('/post', postController.post);
-router.get('/sign-up', userController.signUp );
-router.get('/sign-in', userController.signIn );
+router.get('/sign-up', usersController.signUp );
+router.get('/sign-in', usersController.signIn );
+
+router.post('/create', usersController.create);
 
 module.exports = router;
