51278e87d24e1088452d9c921fe3f4141c1d77ca
BAJPAI <arpit.bajpai@accenture.com>
2022-05-30 23:51:55 +0530

    Finished passport authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -6,17 +6,29 @@ module.exports.profile= function(req, res){
 //    res.end('<h1>User Profile</h1>');
 
     return res.render('user_profile', {
-        title: "Maverick!"
+        title: "Profile Page"
     });
 }
 //  render the sign up page
 module.exports.signUp= function(req, res){
+
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
+
     return res.render('user_sign_up',{
         title: "Codeial | Sign Up"
     });
 }
 // render the sign in page 
 module.exports.signIn= function(req, res){
+
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
+
     return res.render('user_sign_in',{
         title: "Codeial | Sign In"
     });
@@ -51,5 +63,16 @@ module.exports.create = function(req, res){
 
 //  sign in and create the session for the user
 module.exports.createSession = function(req, res){
-    // TODO later
+    return res.redirect('/');
 };
+
+
+module.exports.destroySession = function(req, res, next){
+    req.logout(function(err){
+        if(err){
+            return next(err);
+        }
+    });
+
+    return res.redirect('/');
+}
