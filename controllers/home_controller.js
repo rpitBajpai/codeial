d41c6ff60497345be4d358c5405999e7e1886788
Arpit Bajpai <arpitbajpai4321@gmail.com>
2022-06-09 23:18:53 +0530

    Completed saas project

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -4,6 +4,6 @@ module.exports.home = function(req,res){
 //    return res.end('<h1>Express is up for Codeial!</h1>'); -------sending data to browser directly
 
     return res.render('home', {
-        title : "Home"
+        title : "Home Page"
     });
 };
