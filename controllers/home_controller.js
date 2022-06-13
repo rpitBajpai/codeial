7245864013b7364121e9ab24b4508566850ac723
Arpit Bajpai <arpitbajpai4321@gmail.com>
2022-06-14 00:51:59 +0530

    Display Posts & related User

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,23 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req,res){
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+    
 //    return res.end('<h1>Express is up for Codeial!</h1>'); -------sending data to browser directly
 
-    return res.render('home', {
-        title : "Home Page"
+//  show all the posts
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title : "Codeial | Home",
+    //         posts: posts
+    // });
+    
+//  show all the posts & populate the user of each post
+        Post.find({}).populate('user').exec(function(err, posts){
+            return res.render('home', {
+                title : "Codeial | Home",
+                posts: posts
+        });
     });
 };
