75d80d3d6abf2869812fdbb8432654b8d1083ba3
Arpit Bajpai <arpitbajpai4321@gmail.com>
2022-06-15 01:22:20 +0530

    Nested Population : display Comment & related User

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -15,17 +15,18 @@ module.exports.home = function(req,res){
     // });
     
 //  show all the posts & populate the user of each post
-        Post.find({}).populate('user').exec(function(err, posts){
+        Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        })
+        .exec(function(err, posts){
             return res.render('home', {
                 title : "Codeial | Home",
                 posts: posts
         });
     });
-
-    // Comment.find({}, function(err, comments){
-    //     return res.render('home', {
-    //         title : "Codeial | Home",
-    //         comments: comments
-    //     });
-    // });
 };
