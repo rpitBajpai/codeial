9420b293a8004039a06e83cb3b18eecaa2e627e0
Arpit Bajpai <arpitbajpai4321@gmail.com>
2022-06-16 00:41:46 +0530

    User profile links

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,6 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
+const User = require('../models/user');
 
 module.exports.home = function(req,res){
     // console.log(req.cookies);
@@ -24,9 +25,13 @@ module.exports.home = function(req,res){
             }
         })
         .exec(function(err, posts){
-            return res.render('home', {
-                title : "Codeial | Home",
-                posts: posts
+            User.find({}, function(err, users){
+                return res.render('home', {
+                    title : "Codeial | Home",
+                    posts: posts,
+                    all_users: users
+            });
+            
         });
     });
 };
