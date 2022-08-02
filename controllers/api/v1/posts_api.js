96b6c9e2d5f0808aaa4f3aeb0d140f1ea03928a6
Arpit Bajpai <arpitbajpai4321@gmail.com>
2022-08-02 21:44:43 +0530

    Playing with API::Displaying & Deleting a post

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,7 +1,48 @@
-module.exports.index = function(req, res){
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
+
+module.exports.index = async function(req, res){
+
+
+
+    let posts= await Post.find({})
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
 
     return res.json(200, {
         message: "List of posts",
-        posts: []
+        posts: posts
     })
 }
+
+module.exports.destroy = async function(req,res){
+
+    try{
+        let post= await Post.findById(req.params.id);
+        
+                post.remove();
+                
+                await Comment.deleteMany({post: req.params.id});
+
+                return res.json(200, {
+                    message: "Post & associated Comments deleted successfully!"
+                });
+                
+        // } else{
+        //     req.flash('error', 'You cannot delete this post!');
+        //     return res.redirect('back');
+        // }
+    }catch(err){
+        
+        console.log('*****', err);
+        return res.json(500, {
+            message: "Internal Server Error"
+        });
+    }
+    }
