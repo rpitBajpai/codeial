0c8277449c2d67ba8ded4a35bcd5434403699e9c
Arpit Bajpai <arpitbajpai4321@gmail.com>
2022-08-15 08:29:24 +0530

    Sending our first Email via SMTP

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,5 +1,6 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
+const commentsMailer = require('../mailers/comments_mailer');
 
 module.exports.create = async function(req,res){
 
@@ -17,10 +18,24 @@ module.exports.create = async function(req,res){
                 // save to database after update
                 post.save();
 
+                comment = await comment.populate('user', 'name email').execPopulate();
+                commentsMailer.newComment(comment);
+
+                if(req.xhr){
+
+                    return res.status(200).json({
+                        data: {
+                            comment: comment
+                        },
+                        message: "Post created!"
+                    });
+                }
+                req.flash('success', 'Comment Published!');
                 res.redirect('/');
             };
     }catch(err){
-        console.log('Error', err);
+        // console.log('Error', err);
+        req.flash('error', err);
         return;
     }
     // find whether the post exists, to  which the commnet needs to be added
@@ -40,14 +55,27 @@ module.exports.destroy = async function(req, res){
             comment.remove();
 
             let post= Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+
+            // send the comment ID which was deleted back to the views
+            if(req.xhr){
+                return res.status(200).json({
+                    data: {
+                        comment_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
+                req.flash('success', 'Comment deleted!');
                 return res.redirect('back');
             
         }else{
+            req.flash('error', 'Unauthorized');
             return res.redirect('back');
 
         }
     }catch(err){
-        console.log('Error', err);
+        // console.log('Error', err);
+        req.flash('error', err);
         return;
     }
 }
